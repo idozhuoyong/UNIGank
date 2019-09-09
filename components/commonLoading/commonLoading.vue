cc2221de97ce6178b50d9ff839dff8e09b2b80d6
小卓子 <idozhuoyong@qq.com>
2019-09-09 17:40:50 +0800

    福利

---
+++ components/commonLoading/commonLoading.vue
@@ -0,0 +1,141 @@
+<template>
+	<view class="content">
+		<view class="wrap">
+			<view class="arc"></view>
+			<view class="text"><text>LOADING</text></view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+	
+.content {
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background-color: rgba(0, 0, 0, 0.1);
+	font-family: 'Inconsolata', monospace;
+	overflow: hidden;
+}
+
+.wrap {
+	position: absolute;
+	margin: auto;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	width: 280rpx;
+	height: 300rpx;
+	background: rgba(51, 51, 51, 0.9);
+	opacity: 0.9;
+	border-radius: 15%;
+}
+.arc {
+	position: absolute;
+	margin: auto;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	width: 150rpx;
+	height: 150rpx;
+	border-radius: 50%;
+	border-top: 2px solid #ffea29;
+	border-left: 1px solid transparent;
+	border-right: 1px solid transparent;
+	animation: rt 2s infinite linear;
+}
+.arc::before {
+	position: absolute;
+	margin: auto;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	width: 100rpx;
+	height: 100rpx;
+	border-radius: 50%;
+	border-top: 2px solid #8d29ff;
+	border-left: 1px solid transparent;
+	border-right: 1px solid transparent;
+	animation: rt 4s infinite linear reverse;
+	content: "";
+}
+.arc::after {
+	position: absolute;
+	margin: auto;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	width: 0;
+	height: 0;
+	border-radius: 50%;
+	border-top: initial;
+	border-left: initial;
+	border-right: initial;
+	animation: cw 1.25s infinite;
+	content: "";
+	background: snow;
+}
+
+.text {
+	position: absolute;
+	height: 40rpx;
+	margin: 0 auto;
+	left: 0;
+	right: 0;
+	bottom: 15rpx;
+	text-transform: uppercase;
+	text-align: center;
+	letter-spacing: 0.1em;
+	font-size: 14rpx;
+	font-weight: lighter;
+	color: white;
+}
+.text text {
+	display: none;
+}
+.text::after {
+	animation: txt 3s infinite;
+	content: "";
+}
+
+@keyframes rt {
+	100% {
+		transform: rotate(360deg);
+	}
+}
+@keyframes cw {
+	0% {
+		width: 0;
+		height: 0;
+	}
+	75% {
+		width: 40rpx;
+		height: 40rpx;
+	}
+	100% {
+		width: 0;
+		height: 0;
+	}
+}
+@keyframes txt {
+	0% {
+		content: "LOADING.";
+	}
+	50% {
+		content: "LOADING..";
+	}
+	100% {
+		content: "LOADING...";
+	}
+}
+
+</style>
--- pages/welfare/welfare.vue
+++ pages/welfare/welfare.vue
@@ -8,48 +8,71 @@
 				@click="onHandleCellClick(value)"
 				>
 				<image class="cell-image" 
-					:src="value.url" 
+					src="value.url" 
 					mode="scaleToFill">
 				</image>
 				<view class="cell-text"><text space="nbsp" selectable="true">by: {{value.who}}</text></view>
 			</view>
 		</view>
+		<uni-load-more :status="uniLoadingStatus"></uni-load-more>
+		<!-- 没有数据提示 -->
 		<empty-view v-if="isShowEmptyView" @onHandleReloadData="onHandleReloadData"></empty-view>
+		<!-- 遮罩 -->
+		<common-loading v-if="isShowCommonLoading"></common-loading>
 	</view>
 </template>
 
 <script>
 	import helper from "../../common/helper.js";
 	import EmptyView from "../../components/EmptyView/EmptyView.vue"
+	import commonLoading from '../../components/commonLoading/commonLoading.vue'
+	import uniLoadMore from "@dcloudio/uni-ui/lib/uni-load-more/uni-load-more.vue"
 	
 	export default {
 		components: {
-			EmptyView
+			EmptyView,
+			commonLoading,
+			uniLoadMore
 		},
 		data() {
 			return {
 				isShowEmptyView: false,
+				isShowCommonLoading: false,
+				isPullDownRefresh: false,
+				isReachBottom: false,
 				page: 1,
 				dataOriginArray: [],
 				dataArray: [],
 				cellStyle: "",
 				rowCount: 3 , // 每一行图片的数量
 				isShowPreviewImage: false,
+				uniLoadingStatus: "more"
 			}
 		},
-		onLoad() {
+		onLoad() {			
+			uni.startPullDownRefresh({});
+		},
+		onPullDownRefresh() {
+			// 下拉刷新
+			this.page = 1;
+			this.isPullDownRefresh = true;
 			this.getWelfareData();
 		},
 		onReachBottom() {
 			this.page++;
+			this.isReachBottom = true;
+			this.uniLoadingStatus = "loading";
 			this.getWelfareData();
 		},
 		methods: {
 			onHandleReloadData() {
-				this.isShowEmptyView = false;
-				this.getWelfareData();
+				uni.startPullDownRefresh({});
 			},
 			getWelfareData() {
+				if (this.isPullDownRefresh) {
+					// 下拉刷新显示遮罩
+					this.isShowCommonLoading = true;
+				}
 				uni.request({
 					url: helper.websiteUrl + "data/福利/20/" + this.page,
 					method: 'GET',
@@ -60,30 +83,55 @@
 						console.log(res);
 						if (res.error == false) {
 							// 交易成功
-							let tempIndex = 0;
-							let tempArray = [];
-							for (let value of res.results) {
-								this.dataOriginArray.push(value);
-								
-								if (tempIndex == this.rowCount) {
-									this.dataArray.push(tempArray);
-									tempIndex = 0;
-									tempArray = [];
-								}
-								tempArray.push(value);
-								tempIndex++;
+							this.isShowEmptyView = false;
+							if (this.page == 1) {
+								this.dataOriginArray = [];
 							}
-							this.dataArray.push(tempArray);
+							if (res.results && res.results.length > 0) {
+								this.dataOriginArray = this.dataOriginArray.concat(res.results);
+								
+								let tempIndex = 0;
+								let tempArray = [];
+								let tempDataArray = [];
+								for (let value of this.dataOriginArray) {
+									if (tempIndex == this.rowCount) {
+										tempDataArray.push(tempArray);
+										tempIndex = 0;
+										tempArray = [];
+									}
+									tempArray.push(value);
+									tempIndex++;
+								}
+								tempDataArray.push(tempArray);
+								this.dataArray = tempDataArray;
+							}
 						} else {
 							// 交易失败
-							this.isShowEmptyView = true;
+							if (this.isReachBottom) {
+								this.isReachBottom = false;
+								this.page--;
+							} else {
+								this.isShowEmptyView = true;
+							}
 						}
 					},
 					fail: () => {
 						// 请求失败
-						this.isShowEmptyView = true;
+						if (this.isReachBottom) {
+							this.isReachBottom = false;
+							this.page--;
+						} else {
+							this.isShowEmptyView = true;
+						}
 					},
-					complete: () => {}
+					complete: () => {
+						this.uniLoadingStatus = "more";
+						if (this.isPullDownRefresh) {
+							this.isShowCommonLoading = false; // 隐藏遮罩
+							this.isPullDownRefresh = false;
+							uni.stopPullDownRefresh();
+						}
+					}
 				});
 			},
 			onHandleCellClick(value) {
@@ -107,7 +155,8 @@
 				}
 				uni.previewImage({
 					current: current,
-					urls: urls
+					urls: urls,
+					indicator: "number"
 				})
 			}
 		}
